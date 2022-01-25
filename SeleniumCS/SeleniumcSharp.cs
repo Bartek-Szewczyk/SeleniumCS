063c3b659948be64e97024b96252c49507828a16
Bartek-Szewczyk <ba20001000@gmail.com>
2022-01-25 16:33:26 +0100

    added author, description attribute and screenshot

---
+++ SeleniumCS/SeleniumcSharp.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections;
+
+namespace SeleniumCS
+{
+    [TestFixture]
+    public class SeleniumcSharp
+    {
+        [Test]
+        [Author("Bartek", "bartlomiej.szewczyk@microsoft.wsei.edu.pl")]
+        [Description("Facebook login Verify")]
+        [TestCaseSource("DataDrivenTesting")]
+        public void Test1(string urlName)
+        {
+            IWebDriver driver = null;
+            try
+            {
+                driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();
+                //driver.Url = "https://www.facebook.com/";
+                driver.Url = urlName;
+                //IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+                IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='abcd']"));
+                emailTextField.SendKeys("Selenium C#");
+                driver.Quit();
+            }
+            catch (Exception e)
+            {
+                ITakesScreenshot ts = driver as ITakesScreenshot;
+                Screenshot screenshot = ts.GetScreenshot();
+                screenshot.SaveAsFile("D:\\studia-projekty\\testy i kontrola jakoci oprogramowania\\selenium\\SeleniumCS\\SeleniumCS\\ScreenShots\\s1.jpg", ScreenshotImageFormat.Jpeg);
+                Console.WriteLine(e.StackTrace);
+                throw;
+            }
+            finally
+            {
+                if (driver != null)
+                {
+                    driver.Quit();
+                }
+            }
+           
+        }
+
+        static IList DataDrivenTesting()
+        {
+            ArrayList list = new ArrayList();
+            list.Add("https://www.facebook.com/");
+            //list.Add("https://www.youtube.com/");
+            //list.Add("https://www.gmail.com/");
+            return list;
+        }
+
+        //[Test]
+        //[Author("Bartek", "bartlomiej.szewczyk@microsoft.wsei.edu.pl")]
+        //[Description("Facebook login Verify")]
+        //public void Test2()
+        //{
+        //    IWebDriver driver = new ChromeDriver();
+        //    driver.Manage().Window.Maximize();
+        //    driver.Url = "https://www.facebook.com/";
+        //    IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+        //    emailTextField.SendKeys("Selenium C#");
+        //    driver.Quit();
+        //}
+    }
+}
